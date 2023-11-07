8361bb1d2dc8faa353000107bb0814a543f35db5
Orrin OYCS <cunsi.ouyang@shoncloud.com>
2023-11-07 10:25:41 +0800

    add index page

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+name = "Orrin"
+movies = [
+    {'title': 'a', 'year': '1998'},
+    {'title': 'king of comedy', 'year': '1999'},
+    {'title': 'asfd dfasd ', 'year': '2003'},
+    {'title': 'ef safwarqw ', 'year': '2008'},
+    {'title': 'sdf dfwaf ', 'year': '2002'},
+    {'title': 'yik u tjdj ', 'year': '2004'},
+    {'title': 't ru  urs ', 'year': '2016'}
+]
 
 
 @app.route('/')
 @app.route('/home')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
