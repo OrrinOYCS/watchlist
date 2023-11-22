d8fd5a7904098c0bb8da19cb0092b09c2c4a3801
Orrin <cunsiouyang@gmail.com>
2023-11-22 11:45:16 +0800

    add virtual data and rend on page

--- app.py
+++ app.py
@@ -1,20 +1,70 @@
-from flask import Flask, render_template
+import os.path
+import sys
+import click
+from flask import Flask, render_template, app
+from flask_sqlalchemy import SQLAlchemy
 
+# 判断系统类型决定路径分割线
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+# 初始化flask以及sqlalchemy数据库
 app = Flask(__name__)
-
-name = "Orrin"
-movies = [
-    {'title': 'a', 'year': '1998'},
-    {'title': 'king of comedy', 'year': '1999'},
-    {'title': 'asfd dfasd ', 'year': '2003'},
-    {'title': 'ef safwarqw ', 'year': '2008'},
-    {'title': 'sdf dfwaf ', 'year': '2002'},
-    {'title': 'yik u tjdj ', 'year': '2004'},
-    {'title': 't ru  urs ', 'year': '2016'}
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
+# 定义两张表
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+# 模板渲染数据库中的数据，index写好的html
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movie = Movie.query.all()
+    return render_template('index.html', user=user, movie=movie)
+
+
+@app.cli.command()
+def forge():
+    db.drop_all()
+    db.create_all()
+    name = "Orrin"
+    movies = [
+        {'title': 'jingqiduizhang', 'year': '2023'},
+        {'title': 'shentounaiba', 'year': '2022'},
+        {'title': 'yinhehuweidui3', 'year': '2023'},
+        {'title': 'xiaoshenkedejiushu', 'year': '1997'},
+        {'title': 'aganzhengzhuan', 'year': '1997'},
+        {'title': 'taitannikehao', 'year': '1997'}
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('job done.')
+
+    @app.cli.command()
+    @click.option('--drop', is_flag=True, help='create after drop')
+    def initdb(drop):
+        if drop:
+            db.drop_all()
+        db.create_all()
+        click.echo('initialized database')
