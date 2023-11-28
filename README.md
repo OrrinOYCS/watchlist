68a7a134f484603efed6a4c35ef7a134c7093427
Orrin <cunsiouyang@gmail.com>
2023-11-28 14:10:47 +0800

    movie CRUD

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 <h1>11.23</h1>
 <br>
 <h2>add base.html, pages extend from this page</h2>
+<br>
+<h1>11.28</h1>
+<br>
+<h2>电影数据CRUD实现</h2>
+<br>
+<h3>flash消息</h3>
+<h3>删除按钮一为了安全用input表单POST提交</h3>
+<h3>重定向redirect 记得加url_for</h3>
+<h3>form表单当前页面提交就不用加action，提交到其他页面需要action</h3>
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 import click
-from flask import Flask, render_template, app
+from flask import Flask, render_template, app, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # 判断系统类型决定路径分割线
@@ -15,6 +15,7 @@ else:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
 
@@ -31,12 +32,47 @@ class Movie(db.Model):
 
 
 # 模板渲染数据库中的数据，index写好的html
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = User.query.first()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not title or len(year) > 4 or len(title) > 60:
+            flash("Invalid input!")
+            return redirect(url_for("index"))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash("Insert success!")
+        return redirect(url_for("index"))
     movie = Movie.query.all()
     return render_template('index.html', movie=movie)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash("Invalid input!")
+            return redirect(url_for('edit', movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash("Edit success!")
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash("Delete success!")
+    return redirect(url_for('index'))
 
 
 @app.cli.command()
@@ -77,6 +113,7 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
+# 加了装饰器，作为全局数据随意使用
 @app.context_processor
 def return_user():
     user = User.query.first()
